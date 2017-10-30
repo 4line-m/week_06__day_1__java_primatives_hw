0f575f6b1bae328944ec229408562a39caf2de99
Aline Mokfa <alinemokfa@hotmail.co.uk>
2017-10-30 22:51:32 +0000

    created and passed getName test

--- BusStop.java
+++ BusStop.java
@@ -1,17 +1,13 @@
-// class BusStop {
-//   private String name;
-//   private Person[] passengers;
-//
-//   public BusStop(String name, int capacity){
-//     this.name = name
-//     this.passengers = new Person[capacity];
-//   }
-//
-//   public void movePassengerToBus(Bus bus){
-//     for (int i = 0; i < passengers.length; i++) {
-//       bus.addStudent(passengers[i]);
-//       passengers[i] = null;
-//     }
-//   }
-//
-// }
+class BusStop {
+  private String name;
+  private Person[] passengersQueue;
+
+  public BusStop(String name){
+    this.name = name;
+    this.passengersQueue = new Person[5];
+  }
+
+  public String getName(){
+    return this.name;
+  }
+}
--- BusStopTest.java
+++ BusStopTest.java
@@ -1,9 +1,23 @@
-// import org.junit.*;
-// import static org.junit.Assert.assertEquals;
-//
-// public class BusStopTest{
-//
-//   Bus bus;
-//   Person passenger;
-//   BusStop busStop;
-//
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+
+public class BusStopTest{
+
+  BusStop busStop;
+  Bus bus;
+  Person passenger;
+
+  @Before
+  public void before(){
+    this.busStop = new BusStop("Waverley Station");
+    this.bus = new Bus(22);
+    this.passenger = new Person();
+  }
+
+  @Test
+  public void hasName(){
+    String name = busStop.getName();
+    assertEquals("Waverley Station", name);
+  }
+
+}
